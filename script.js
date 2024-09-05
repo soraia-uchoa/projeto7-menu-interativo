3847ee777600bd1e7f449a562bddad4c928193fc
soraia-uchoa <soraiavercosa3@icloud.com>
2024-09-04 21:01:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let option;
+do {
+    opcao = prompt(`
+        Escolha uma opção:
+        1. Opção 1
+        2. Opção 2
+        3. Opção 3
+        4. Opção 4
+        5. Encerrar`)
+}
+
+switch(option) {
+    case '1' :
+        alert('Você escolheu a primeira opção'.')
+        case
